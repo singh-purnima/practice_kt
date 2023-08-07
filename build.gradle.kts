cbc1158e52a666bcbfee386f3f59f44b8fe07478
Purnima <er.purnimasingh11@gmail.com>
2023-08-07 11:05:22 +0530

    skeleton project

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+tasks.test {
+    useTestNG()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-testng"))
+}
